132b4aebd60702fa7bd9c64f1c2f6bc41b9972e4
Stacey Koornneef <stacey.koornneef@uoit.net>
2018-05-05 02:25:12 +0000

    not recognising function names

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ from guizero import App, PushButton, Text
 import RPi.GPIO as GPIO
 import time
 import os
-from program import *
+from program.py import *
 
 def write(filename):
     file = open(filename,"w")
@@ -10,7 +10,7 @@ def write(filename):
     file.write("import RPi.GPIO as GPIO\n")
     file.write("import time\n")
     file.write("GPIO.setmode(GPIO.BOARD)\n")
-    file.write("GPIO.setup(12, GPIO.OUT\n")
+    file.write("GPIO.setup(12, GPIO.OUT)\n")
     file.write("def program():\n")
     file.write("\tGPIO.output(12, GPIO.HIGH)\n")
     file.write("\ttime.sleep(1)\n")
