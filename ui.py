f50a5936ab0b33361a56c14b5a62923b77aa9c78
Stacey Koornneef <stacey.koornneef@uoit.net>
2018-05-05 02:13:09 +0000

    writes code to a python file that will flash light

---
+++ ui.py
@@ -0,0 +1,24 @@
+from guizero import App, PushButton, Text
+import RPi.GPIO as GPIO
+import time
+import os
+from program import *
+
+def write(filename):
+    file = open(filename,"w")
+
+    file.write("import RPi.GPIO as GPIO\n")
+    file.write("import time\n")
+    file.write("GPIO.setmode(GPIO.BOARD)\n")
+    file.write("GPIO.setup(12, GPIO.OUT\n")
+    file.write("def program():\n")
+    file.write("\tGPIO.output(12, GPIO.HIGH)\n")
+    file.write("\ttime.sleep(1)\n")
+    file.write("\tGPIO.output(12, GPIO.LOW)\n")
+    file.close()
+
+
+filename = "program.py"
+write(filename)
+program()
+print("DONE")
