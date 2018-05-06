73d54b89f01c0b59790aff0add1bf345f0c245be
Stacey Koornneef <stacey.koornneef@uoit.net>
2018-05-06 14:16:32 +0000

    refactored

--- ui.py
+++ ui.py
@@ -3,10 +3,9 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-def write():
+def openFile():
     print("WRITING")
-    file = open("program.py","w")
-
+    #file = open("program.py", "w")
     file.write("import RPi.GPIO as GPIO\n")
     file.write("import time\n")
     file.write("GPIO.setmode(GPIO.BOARD)\n")
@@ -14,20 +13,21 @@ def write():
     file.write("GPIO.output(12, GPIO.HIGH)\n")
     file.write("time.sleep(1)\n")
     file.write("GPIO.output(12, GPIO.LOW)\n")
-    file.write("GPIO.cleanup()")
-    file.close()
     print("WRITTEN")
 
 def run():
+    file.write("GPIO.cleanup()\n")
+    file.close()
     print("RUNNING")
     os.system("python ./program.py")
     print("RAN")
 
 
-filename = "program.py"
 
 
 app = App(title = "Science Rendezvous May 2018")
-writeButton = PushButton(app, command = write, text = "Write File")
+file = open("program.py","w")
+#writeButton = PushButton(app, command = write, text = "Write File")
 runButton = PushButton(app, command = run, text = "Run Program")
+startButton = PushButton(app, command = openFile, text = "Start New Program")
 app.display()
